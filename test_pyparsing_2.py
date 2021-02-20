65a8df81c6b78c5e42e447f381e0869d5ca7d0e5
luluci <lunacied@gmail.com>
2021-02-20 12:38:28 +0900

    fix example

--- test_pyparsing_2.py
+++ test_pyparsing_2.py
@@ -8,20 +8,27 @@ def pa_identifier(match):
 
 
 pp_rule1 = pp.Keyword("int").setParseAction(pa_keyword)
-pp_rule2 = pp.Keyword("static").setParseAction(pa_keyword)
 pp_identifier = pp.Word(pp.alphas+"_", pp.alphanums+"_").setParseAction(pa_identifier)
 pp_type_decl = (
 	pp.Optional(pp_rule1)
-	& pp.Optional(pp_rule2)
 	& pp_identifier
 )
 
 input = [
 	"int",
 	"init",
-	"static",
 ]
 for i, inp in enumerate(input):
 	ret = pp_type_decl.parseString(inp)
 	print("[" + str(i) + "]")
 	print(ret)
+
+"""
+$ result
+identifier: ['int']
+[0]
+['int']
+identifier: ['init']
+[1]
+['init']
+"""
